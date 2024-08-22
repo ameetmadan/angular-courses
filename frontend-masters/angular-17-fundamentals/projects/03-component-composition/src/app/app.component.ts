7511a161f6f091282ac8a09f4a568c9cf7bd126e
ameetmadan <ameetmadan@gmail.com>
2024-08-22 09:21:34 +0200

    04

--- frontend-masters/angular-17-fundamentals/projects/03-component-composition/src/app/app.component.ts
+++ frontend-masters/angular-17-fundamentals/projects/03-component-composition/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { UserinfoComponent } from "./userinfo/userinfo.component";
 
 @Component({
   selector: 'app-root',
+  imports: [UserinfoComponent],
   standalone: true,
   template: `
     <section class="menu">🏠 Home</section>
     <section class="main">
       <h1>Metrics and Figures Dashboard</h1>
       <section class="content">
-        <article class="tile"></article>
+        <article class="tile"><app-userinfo></app-userinfo></article>
         <article class="tile">
           <img src="/assets/noun-pie-chart-6331100-C462DD.png" height="300" />
         </article>
